77d4fbc12fb7f229be15c159d68db364da2620cb
Kyle Roehrs <kyleroehrs@yahoo.com>
2020-12-20 08:53:14 -0800

    initial project commit

---
+++ src/App.js
@@ -0,0 +1,250 @@
+// import react
+import React, { useState } from 'react';
+
+// import mapbox react library
+import ReactMapGl, { Marker, Popup } from 'react-map-gl';
+
+// import water icon
+import { IoIosWater } from 'react-icons/io';
+
+// import firebase file
+import fire from './firebase';
+
+export default function App() {
+    // store map styles
+    const normalView = 'mapbox://styles/kyleroehrs/ckimhjmvt13cp17oc1gkcrqrh';
+    const streetView = 'mapbox://styles/kyleroehrs/ckimi49y22qmo17o4l9d9g80u';
+
+    // set starting veiwport to be passed to map component
+    const [viewport, setViewport] = useState({
+        width: '100vw',
+        height: '100vh',
+        latitude: 37.691388,
+        longitude: -121.89472,
+        zoom: 14,
+    });
+
+    // hook to change  map style
+    const [mapStyle, setMapStyle] = useState(normalView);
+
+    // hook to hide markers
+    const [showMarkers, setShowMarkers] = useState(true);
+
+    // hook to store markers being placed
+    const [markers, setMarkers] = useState([]);
+
+    // initial state from database
+    const dataSet = [];
+
+    // set up database
+    const db = fire.firestore();
+
+    // query markers and populate starting state
+    const loadDb = () => {
+        console.log('fired');
+        db.collection('markers')
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    dataSet.push({
+                        lng: doc.data().lng,
+                        lat: doc.data().lat,
+                        key: doc.id,
+                        keyName: doc.id,
+                    });
+                });
+            })
+            .then(() => {
+                setMarkers(dataSet);
+            });
+    };
+
+    // post to database
+    const postToDb = (e) => {
+        db.collection('markers')
+            .add({
+                lng: e.lngLat[0],
+                lat: e.lngLat[1],
+            })
+            .then(function (docRef) {
+                setMarkers((prev) => {
+                    return [
+                        ...prev,
+                        {
+                            lng: e.lngLat[0],
+                            lat: e.lngLat[1],
+                            key: docRef.id,
+                            keyName: docRef.id,
+                        },
+                    ];
+                });
+            })
+            .catch(function (error) {
+                console.error('Error adding document: ', error);
+            });
+    };
+
+    return (
+        <div>
+            {/* {button to toggle map style} */}
+            <button
+                onClick={() => {
+                    setMapStyle(
+                        mapStyle === normalView ? streetView : normalView
+                    );
+                }}
+            >
+                {mapStyle === normalView ? 'Street View' : 'Normal View'}
+            </button>
+
+            {/* button to show and hide markers */}
+            <button
+                onClick={() => {
+                    setShowMarkers((prev) => !prev);
+                    console.log(markers);
+                }}
+            >
+                {showMarkers ? 'Hide Markers' : 'Show Markers'}
+            </button>
+            {/* spread base viewport, update as dragged, pass in hook for map style, callback to add markers when clicked, place markers after load */}
+            <ReactMapGl
+                {...viewport}
+                onLoad={() => {
+                    loadDb();
+                }}
+                onViewportChange={(nextViewport) => setViewport(nextViewport)}
+                mapboxApiAccessToken="pk.eyJ1Ijoia3lsZXJvZWhycyIsImEiOiJja2hmbXl0bGYwMHF0Mndxazg0MjZ0aHhmIn0.jjFVj0sjEYtpHijUGRxcUw"
+                mapStyle={mapStyle}
+                onClick={(e) => {
+                    postToDb(e);
+                }}
+            >
+                {/* map markers to the dom, hides them when hide button clicked */}
+                {markers.map((marker) => {
+                    return (
+                        <Marker
+                            latitude={marker.lat}
+                            longitude={marker.lng}
+                            key={marker.key}
+                            keyName={marker.keyName}
+                            draggable={true}
+                            offsetLeft={-8}
+                            offsetTop={-10}
+                            onDragEnd={(event) => {
+                                let myList = markers.filter((cur) => {
+                                    return marker.keyName !== cur.keyName;
+                                });
+                                setMarkers(() => {
+                                    return [
+                                        ...myList,
+                                        {
+                                            lng: event.lngLat[0],
+                                            lat: event.lngLat[1],
+                                            key: marker.keyName,
+                                            keyName: marker.keyName,
+                                        },
+                                    ];
+                                });
+
+                                db.collection('markers')
+                                    .doc(marker.keyName)
+                                    .set({
+                                        lng: event.lngLat[0],
+                                        lat: event.lngLat[1],
+                                    });
+                            }}
+                        >
+                            <IoIosWater
+                                style={!showMarkers && { display: 'none' }}
+                            />
+                        </Marker>
+                    );
+                })}
+                {markers.map((marker) => {
+                    return (
+                        <Popup
+                            latitude={marker.lat}
+                            longitude={marker.lng}
+                            key={marker.key + 'popup'}
+                            closeButton={true}
+                            closeOnClick={true}
+                            anchor="bottom"
+                            dynamicPosition={false}
+                            offsetTop={-13}
+                        >
+                            Testing
+                        </Popup>
+                    );
+                })}
+            </ReactMapGl>
+        </div>
+    );
+}
+
+// import {
+//     Editor,
+//     EditingMode,
+//     DrawLineStringMode,
+//     DrawPolygonMode,
+//     onUpdate,
+// } from 'react-map-gl-draw';
+
+//     const [modeIdHook, setModeIdHook] = useState(null);
+
+//     const [modeHandlerHook, setModeHandlerHook] = useState(null);
+
+//     const MODES = [
+//         {
+//             id: 'drawPolyline',
+//             text: 'Draw Polyline',
+//             handler: DrawLineStringMode,
+//         },
+//         { id: 'drawPolygon', text: 'Draw Polygon', handler: DrawPolygonMode },
+//         { id: 'editing', text: 'Edit Feature', handler: EditingMode },
+//     ];
+
+//     const switchMode = (e) => {
+//         const modeId = e.target.value === modeIdHook ? null : e.target.value;
+//         const mode = MODES.find((m) => m.id === modeId);
+//         const modeHandler = mode ? new mode.handler() : null;
+
+//         setModeIdHook(modeId);
+//         setModeHandlerHook(modeHandler);
+//     };
+
+//     const renderToolbar = () => {
+//         return (
+//             <div
+//                 style={{
+//                     position: 'absolute',
+//                     top: 0,
+//                     right: 0,
+//                     maxWidth: '320px',
+//                 }}
+//             >
+//                 <select onChange={switchMode}>
+//                     <option value="">--Please choose a draw mode--</option>
+//                     {MODES.map((mode) => (
+//                         <option key={mode.id} value={mode.id}>
+//                             {mode.text}
+//                         </option>
+//                     ))}
+//                 </select>
+//             </div>
+//         );
+//     };
+
+//     return (
+//
+
+//                 <Editor
+//                     // to make the lines/vertices easier to interact with
+//                     clickRadius={12}
+//                     mode={modeHandlerHook}
+//                     onSelect={(_) => {}}
+//                 />
+//                 {renderToolbar()}
+//             </ReactMapGl>
+//         </div>
+//     );
+// }
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// import firebase
+import Firebase from 'firebase';
+
+let firebaseConfig = {
+    apiKey: 'AIzaSyAMjzgS2oy3eOYqx7xuLarPH4F3CG66W6c',
+    authDomain: 'mapbox-project-e8d59.firebaseapp.com',
+    projectId: 'mapbox-project-e8d59',
+    storageBucket: 'mapbox-project-e8d59.appspot.com',
+    messagingSenderId: '88701336013',
+    appId: '1:88701336013:web:15c7fa34a737079a946c84',
+};
+// Initialize Firebase
+export default Firebase.initializeApp(firebaseConfig);
